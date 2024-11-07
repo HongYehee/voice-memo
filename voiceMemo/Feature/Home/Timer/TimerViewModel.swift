8c2ec65e39b9e86be5e1cb9b01fd21620448724d
yh.hong <ghddpgml@gmail.com>
2024-11-07 15:22:09 +0900

    타이머 - Local Notification 추가

--- voiceMemo/Feature/Home/Timer/TimerViewModel.swift
+++ voiceMemo/Feature/Home/Timer/TimerViewModel.swift
@@ -2,6 +2,7 @@
 //  voiceMemo
 
 import Foundation
+import UIKit
 
 class TimerViewModel: ObservableObject {
     @Published var isDisplaySetTimeView: Bool
@@ -9,19 +10,22 @@ class TimerViewModel: ObservableObject {
     @Published var timer: Timer?
     @Published var timeRemaining: Int
     @Published var isPaused: Bool
+    var notificationService: NotificationService
     
     init(
         isDisplaySetTimeView: Bool = true,
         time: Time = .init(hours: 0, minutes: 0, seconds: 0),
         timer: Timer? = nil,
         timeRemaining: Int = 0,
-        isPaused: Bool = false
+        isPaused: Bool = false,
+        notificationService: NotificationService = .init()
     ) {
         self.isDisplaySetTimeView = isDisplaySetTimeView
         self.time = time
         self.timer = timer
         self.timeRemaining = timeRemaining
         self.isPaused = isPaused
+        self.notificationService = notificationService
     }
 }
 
@@ -52,6 +56,14 @@ private extension TimerViewModel {
     func startTimer() {
         guard timer == nil else { return }
         
+        var backgroundTaskId: UIBackgroundTaskIdentifier?
+        backgroundTaskId = UIApplication.shared.beginBackgroundTask {
+            if let task = backgroundTaskId {
+                UIApplication.shared.endBackgroundTask(task)
+                backgroundTaskId = .invalid
+            }
+        }
+        
         timer = Timer.scheduledTimer(
             withTimeInterval: 1,
             repeats: true
@@ -60,6 +72,12 @@ private extension TimerViewModel {
                 self.timeRemaining -= 1
             } else {
                 self.stopTimer()
+                self.notificationService.sendNotification()
+                
+                if let task = backgroundTaskId {
+                    UIApplication.shared.endBackgroundTask(task)
+                    backgroundTaskId = .invalid
+                }
             }
         }
     }
